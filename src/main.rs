05dcbad1cdf65e5dd8d3937d94184b9dd5191c83
Gustavo Araujo <gustavo.araujo@devsquad.com>
2023-01-08 22:18:47 -0300

    fix: Refactory function to search package

--- src/main.rs
+++ src/main.rs
@@ -18,42 +18,22 @@ async fn main() {
     for (key, value) in file.require {
         let package_name = key.to_string();
         if key != "php" && !key.contains("ext") {
-            let package_manager_info = get_package_info(&package_name).await;
+            let package_manager_info = search_composer_package(&package_name).await;
 
-            //TODO Create function at string to get that info
             let version_info =
                 search_valid_version(&value, &package_manager_info.package.versions).unwrap();
 
-            let license = version_info
-                .license
-                .clone()
-                .into_iter()
-                .nth(0)
-                .expect("Not found license");
-
-            result_file
-                .serialize(PackageRow {
-                    name: package_manager_info.package.name,
-                    description: package_manager_info.package.description,
-                    copyright: license.to_string(),
-                    license: license,
-                    version: version_info.version.to_string(),
-                    impementation_description: String::from("installed with composer at project"),
-                    path: format!("/vendor/{}", &package_name),
-                    reference: package_manager_info.package.repository,
-                    language: package_manager_info.package.language,
-                    install: String::from("composer"),
-                })
-                .unwrap();
+            let package_row =
+                composer_package_row(&package_manager_info, version_info, &package_name);
+            result_file.serialize(package_row).unwrap();
         }
     }
 
     for (key, value) in file.require_dev {
         let package_name = key.to_string();
         if key != "php" && !key.contains("ext") && key != "enlightn/enlightnpro" {
-            let package_manager_info = get_package_info(&package_name).await;
+            let package_manager_info = search_composer_package(&package_name).await;
 
-            //TODO Create function at string to get that info
             let version_info = search_valid_version(&value, &package_manager_info.package.versions)
                 .expect("Invalid versionFailed to get version");
 
@@ -64,20 +44,9 @@ async fn main() {
                 .nth(0)
                 .expect("Not found license");
 
-            result_file
-                .serialize(PackageRow {
-                    name: package_manager_info.package.name,
-                    description: package_manager_info.package.description,
-                    copyright: license.to_string(),
-                    license: license,
-                    version: version_info.version.to_string(),
-                    impementation_description: String::from("installed with composer at project"),
-                    path: format!("/vendor/{}", &package_name),
-                    reference: package_manager_info.package.repository,
-                    language: package_manager_info.package.language,
-                    install: String::from("composer"),
-                })
-                .unwrap();
+            let package_row =
+                composer_package_row(&package_manager_info, version_info, &package_name);
+            result_file.serialize(package_row).unwrap();
         }
     }
 
@@ -91,7 +60,7 @@ fn read_file() -> ComposerPackage {
     serde_json::from_str::<ComposerPackage>(&string_file).unwrap()
 }
 
-async fn get_package_info(package_name: &String) -> PackagistResponse {
+async fn search_composer_package(package_name: &String) -> PackagistResponse {
     let url = format!("https://repo.packagist.org/packages/{}.json", package_name);
 
     let res = reqwest::get(url)
@@ -142,4 +111,30 @@ fn search_valid_version<'a>(
     Err(VersionNotFound {
         message: String::from("Version not found"),
     })
+}
+
+fn composer_package_row(
+    package_manager_info: &PackagistResponse,
+    version_info: &PackagistVersion,
+    package_name: &String,
+) -> PackageRow {
+    let license = version_info
+        .license
+        .clone()
+        .into_iter()
+        .nth(0)
+        .expect("Not found license");
+
+    PackageRow {
+        name: package_manager_info.package.name.to_string(),
+        description: package_manager_info.package.description.to_string(),
+        copyright: license.to_string(),
+        license: license,
+        version: version_info.version.to_string(),
+        impementation_description: String::from("installed with composer at project"),
+        path: format!("/vendor/{}", package_name),
+        reference: package_manager_info.package.repository.to_string(),
+        language: package_manager_info.package.language.to_string(),
+        install: String::from("composer"),
+    }
 }
