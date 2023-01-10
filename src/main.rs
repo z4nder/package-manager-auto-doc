bee3f4a588ce1d30d21aa76afa822d1fa48b0532
Gustavo Araujo <gustavo.araujo@devsquad.com>
2023-01-09 21:29:50 -0300

    fit: User user input

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,49 @@
 use csv::Writer;
 use std::{collections::HashMap, fs::File};
 mod types;
-
 use crate::types::{
     ComposerPackage, NpmMetaData, NpmResponse, PackageJson, PackageRow, PackagistResponse,
     PackagistVersion, VersionNotFound,
 };
-
-const RESULT_FILE_PATH: &str = "/home/gadsdev/projects/rust/generate-libs-docs/files/result.csv";
-const COMPOSER_FILE_PATH: &str =
-    "/home/gadsdev/projects/rust/generate-libs-docs/files/composer.json";
-const PACKAGE_JSON_FILE_PATH: &str =
-    "/home/gadsdev/projects/rust/generate-libs-docs/files/package.json";
+use question::{Answer, Question};
+use std::fs;
+use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() {
-    let composer_json = read_composer_json(COMPOSER_FILE_PATH);
+    let srcdir = PathBuf::from("./files");
+    let absolute_path = fs::canonicalize(&srcdir)
+        .expect("Faile to get current path")
+        .into_os_string()
+        .into_string()
+        .unwrap();
 
-    let package_json = read_package_json(PACKAGE_JSON_FILE_PATH);
+    let result_file_path = format!("{}/result.csv", absolute_path);
+    let mut result_file = Writer::from_path(result_file_path).unwrap();
 
-    let mut result_file = Writer::from_path(RESULT_FILE_PATH).unwrap();
+    let doc_composer = Question::new("Do you want to document composer.json?")
+        .default(Answer::YES)
+        .show_defaults()
+        .confirm();
 
-    insert_composer_data_in_to_csv(composer_json.require, &mut result_file).await;
-    insert_composer_data_in_to_csv(composer_json.require_dev, &mut result_file).await;
+    if doc_composer == Answer::YES {
+        let compsoer_file_path = format!("{}/composer.json", absolute_path);
+        let composer_json = read_composer_json(&compsoer_file_path);
+        insert_composer_data_in_to_csv(composer_json.require, &mut result_file).await;
+        insert_composer_data_in_to_csv(composer_json.require_dev, &mut result_file).await;
+    }
 
-    insert_package_json_data_in_to_csv(package_json.dependencies, &mut result_file).await;
-    insert_package_json_data_in_to_csv(package_json.dev_dependencies, &mut result_file).await;
+    let doc_package_json = Question::new("Do you want to document package.json?")
+        .default(Answer::YES)
+        .show_defaults()
+        .confirm();
+
+    if doc_package_json == Answer::YES {
+        let package_json_file_path = format!("{}/package.json", absolute_path);
+        let package_json = read_package_json(&package_json_file_path);
+        insert_package_json_data_in_to_csv(package_json.dependencies, &mut result_file).await;
+        insert_package_json_data_in_to_csv(package_json.dev_dependencies, &mut result_file).await;
+    }
 }
 
 fn read_composer_json(file_path: &str) -> ComposerPackage {
